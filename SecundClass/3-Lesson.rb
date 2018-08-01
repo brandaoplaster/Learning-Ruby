8032f8557895d49619ef567bfdb3a3ac0f8a9202
Brandao Plaster <brandaoplaster@gmail.com>
2018-08-01 15:44:21 -0300

    Concluido a 3 atividade da aula 2. receber dois valores inteiros e realizar a soma dele e mostra no terminal.

---
+++ SecundClass/3-Lesson.rb
@@ -0,0 +1,15 @@
+
+print "Informe o primeiro numero inteiro.: "
+
+#recebendo o primeiro numero
+number1 = gets.chomp.to_i
+
+print "Informe o segundo numero inteiro.: "
+
+#recebendo o segundo numero
+number2 = gets.to_i
+
+addition = number1 + number2
+
+#exebindo o resultado no terminal
+puts "O resultado da soma entre os numeros eh #{addition}"
