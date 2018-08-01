e6714dae3a9fd817708253e44443a2e78420750b
Brandao Plaster <brandaoplaster@gmail.com>
2018-08-01 15:50:22 -0300

    Concluido tarefa 2, informa a idade e o nome e mostra no console.

---
+++ Tasks/2-Task.rb
@@ -0,0 +1,12 @@
+print "Informe a sua idade.: "
+
+#recebendo a entrada de dados do teclado
+age = gets.chomp.to_i
+
+print "Informe o seu nome.: "
+
+#recebendo a entrada de dados do teclado
+name = gets.chomp
+
+#exibindo os dados no console
+puts "Meu nome eh #{name} e minha idade eh #{age} anos!"
